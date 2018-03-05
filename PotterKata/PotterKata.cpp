aa3e1983aeb09d5e36f845e2d8de598451e923f1
vavrinchen <vavrinlive@gmail.com>
2018-03-05 14:58:52 +0800

    [M] 1.Removing 'using namespace std'

--- PotterKata/PotterKata.cpp
+++ PotterKata/PotterKata.cpp
@@ -7,12 +7,10 @@
 #include <map>
 #include <limits>
 
-using namespace std;
 
 const int PRICE_SINGLE_BOOK = 100;
 const int MAX_EPISODE_NUM = 5;
-const int BUNCH_ONE_EPISODES = 1;
-const int BUNCH_TWO_EPISODES = 2;
+
 const int BUNCH_THREE_EPISODES = 3;
 const int BUNCH_FOUR_EPISODES = 4;
 const int BUNCH_FIVE_EPISODES = 5;
@@ -20,9 +18,9 @@ const int BUNCH_FIVE_EPISODES = 5;
 
 
 
-map<int, int> ShoppingCart::get_checkout_sequence_by_most_episodes(vector<int> &shopping_list)
+std::map<int, int> ShoppingCart::get_checkout_sequence_by_most_episodes(std::vector<int> &shopping_list)
 {
-	map<int, int> checkout_sequence;
+	std::map<int, int> checkout_sequence;
 	sort(shopping_list.begin(), shopping_list.end());
 	int min_book_num = 0;
 
@@ -39,7 +37,7 @@ map<int, int> ShoppingCart::get_checkout_sequence_by_most_episodes(vector<int> &
 }
 
 
-bool ShoppingCart::check_exist_bunch_5_episodes_and_bunch_3_episodes(map<int, int> &checkout_sequence)
+bool ShoppingCart::check_exist_bunch_5_episodes_and_bunch_3_episodes(std::map<int, int> &checkout_sequence)
 {
 	if (checkout_sequence[BUNCH_FIVE_EPISODES] > 0 && checkout_sequence[BUNCH_THREE_EPISODES] > 0)
 	{
@@ -49,18 +47,18 @@ bool ShoppingCart::check_exist_bunch_5_episodes_and_bunch_3_episodes(map<int, in
 	return false;
 }
 
-void ShoppingCart::replace_bunch_5_episodes_and_bunch_3_episodes_to_bunch_4_episodes(map<int, int> &checkout_sequence)
+void ShoppingCart::replace_bunch_5_episodes_and_bunch_3_episodes_to_bunch_4_episodes(std::map<int, int> &checkout_sequence)
 {
 	int add_to_bunch_four_num = 0;
 
-	add_to_bunch_four_num = min(checkout_sequence[BUNCH_FIVE_EPISODES], checkout_sequence[BUNCH_THREE_EPISODES]);
+	add_to_bunch_four_num = std::min(checkout_sequence[BUNCH_FIVE_EPISODES], checkout_sequence[BUNCH_THREE_EPISODES]);
 	checkout_sequence[BUNCH_FIVE_EPISODES] -= add_to_bunch_four_num;
 	checkout_sequence[BUNCH_THREE_EPISODES] -= add_to_bunch_four_num;
 	checkout_sequence[BUNCH_FOUR_EPISODES] += add_to_bunch_four_num * 2;
 }
 
 
-map<int, int> ShoppingCart::produce_more_bunch_4_episodes_sequences(map<int, int> &checkout_sequence)
+std::map<int, int> ShoppingCart::produce_more_bunch_4_episodes_sequences(std::map<int, int> &checkout_sequence)
 {
 	if (check_exist_bunch_5_episodes_and_bunch_3_episodes(checkout_sequence))
 	{
@@ -70,22 +68,22 @@ map<int, int> ShoppingCart::produce_more_bunch_4_episodes_sequences(map<int, int
 	return checkout_sequence;
 }
 
-map<int, int> ShoppingCart::get_cheapest_checkout_sequence(vector<int> &shopping_list)
+std::map<int, int> ShoppingCart::get_cheapest_checkout_sequence(std::vector<int> &shopping_list)
 {
-	map<int, int> checkout_sequence = get_checkout_sequence_by_most_episodes(shopping_list);
-	map<int, int> cheapest_sequence = produce_more_bunch_4_episodes_sequences(checkout_sequence);
+	std::map<int, int> checkout_sequence = get_checkout_sequence_by_most_episodes(shopping_list);
+	std::map<int, int> cheapest_sequence = produce_more_bunch_4_episodes_sequences(checkout_sequence);
 	return cheapest_sequence;
 }
 
-int ShoppingCart::checkout(vector<int> &shopping_list)
+int ShoppingCart::checkout(std::vector<int> &shopping_list)
 {
 
 	float discount_type[] = { 1, 0.95, 0.9, 0.8, 0.75 };
-	float total_price = 0;
+	int total_price = 0;
 
-	map<int, int> cheapest_sequence = get_cheapest_checkout_sequence(shopping_list);
+	std::map<int, int> cheapest_sequence = get_cheapest_checkout_sequence(shopping_list);
 
-	for (map<int, int>::iterator it = cheapest_sequence.begin(); it != cheapest_sequence.end(); ++it)
+	for (std::map<int, int>::iterator it = cheapest_sequence.begin(); it != cheapest_sequence.end(); ++it)
 	{
 		total_price += PRICE_SINGLE_BOOK * it->first* it->second * discount_type[it->first - 1];
 	}
--- PotterKata/PotterKata.h
+++ PotterKata/PotterKata.h
@@ -2,20 +2,20 @@
 
 #include <map>
 #include <vector>
-using namespace std;
 
 
 class ShoppingCart
 {
+
 private:
-	map<int, int> get_checkout_sequence_by_most_episodes(vector<int> &shopping_list);
-	bool check_exist_bunch_5_episodes_and_bunch_3_episodes(map<int, int> &checkout_sequence);
-	void replace_bunch_5_episodes_and_bunch_3_episodes_to_bunch_4_episodes(map<int, int> &checkout_sequence);
-	map<int, int> produce_more_bunch_4_episodes_sequences(map<int, int> &checkout_sequence);
-	map<int, int> get_cheapest_checkout_sequence(vector<int> &shopping_list);
+	std::map<int, int> get_checkout_sequence_by_most_episodes(std::vector<int> &shopping_list);
+	bool check_exist_bunch_5_episodes_and_bunch_3_episodes(std::map<int, int> &checkout_sequence);
+	void replace_bunch_5_episodes_and_bunch_3_episodes_to_bunch_4_episodes(std::map<int, int> &checkout_sequence);
+	std::map<int, int> produce_more_bunch_4_episodes_sequences(std::map<int, int> &checkout_sequence);
+	std::map<int, int> get_cheapest_checkout_sequence(std::vector<int> &shopping_list);
 
 public:
-	int checkout(vector<int> &shopping_list);
+	int checkout(std::vector<int> &shopping_list);
 
 	ShoppingCart() = default;
 	~ShoppingCart() = default;
